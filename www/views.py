544d13fd32386442c890878ebfb9b1e10b9a3eff
Kyle Cooke <cookekyle99@yahoo.co.uk>
2019-03-10 19:39:17 +0000

    fix manager not accepting oauth tokens as authentication

--- www/views.py
+++ www/views.py
@@ -8,8 +8,7 @@ from django.contrib.auth import login
 # Create your views here.
 def authorized(request):
     if request.GET.get('code'):
-        protocol = "http" if DEBUG else "https"
-        response = requests.post('{}://{}/o/token/'.format(protocol, MANAGER_URL),
+        response = requests.post('http://manager:9090/o/token/',
                                  data={
                                      'code': request.GET.get('code'),
                                      'grant_type': 'authorization_code',
@@ -17,10 +16,11 @@ def authorized(request):
                                      'client_secret': CLIENT_SECRET,
                                  })
         headers = {'Authorization': 'Bearer {}'.format(response.json()['access_token'])}
-        response = requests.get('{}://{}/api/profile'.format(protocol, MANAGER_URL),
+        response = requests.get('http://manager:9090/api/profile',
                                 headers=headers)
+        print(response.content)
         r = response.json()
-        my_slug = ALLOWED_HOSTS.split('.', 1)[0]
+        my_slug = ALLOWED_HOSTS[0].split('.', 1)[0]
         if my_slug in r['slugs']:
             # get or create user
             u = User.objects.get_or_create(is_superuser=True, is_staff=True, username=r['username'])
